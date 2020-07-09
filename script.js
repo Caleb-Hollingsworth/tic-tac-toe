d74427a9668e091a586d0a0af3c6f450976926be
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-07-09 16:20:44 -0500

    adding a few changes

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ let player1 = 'X';
 let player2 = 'O';
 let currentPlayer = player1;
 let playerTurn = document.querySelector('.playerTurn');
-const squareChoice = document.querySelectorAll('.grid-item');
-for (let i = 0; i < squareChoice.length; i++) {
-	squareChoice[i].position = i + 1;
-	squareChoice[i].addEventListener('click', checkBoxNotOccupied);
+let gameBoard = document.querySelector('.gameboard');
+const box = document.querySelectorAll('.grid-item');
+for (let i = 0; i < box.length; i++) {
+	box[i].position = i;
+	box[i].addEventListener('click', checkBoxNotOccupied);
 }
+const restartButton = document.querySelector('.restartButton');
+restartButton.addEventListener('click', startGame);
+let winConditions = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
 
 function checkWinCondition() {
 	//here we will check for win conditions
@@ -20,7 +33,7 @@ function checkTieCondition() {
 function switchPlayers() {
 	//here we will switch between `x` and `o`
 	currentPlayer = currentPlayer === player1 ? player2 : player1;
-	if (currentPlayer === player1) {
+	if (currentPlayer === player2) {
 		playerTurn.innerText = "Player 2 it's your turn!";
 	} else {
 		playerTurn.innerText = "Player 1 it's your turn!";
@@ -53,6 +66,15 @@ function placeToken() {
 function startGame() {
 	//self explanatory
 	//possible restart operation
+	document.getElementById('1').innerHTML = '';
+	document.getElementById('2').innerHTML = '';
+	document.getElementById('3').innerHTML = '';
+	document.getElementById('4').innerHTML = '';
+	document.getElementById('5').innerHTML = '';
+	document.getElementById('6').innerHTML = '';
+	document.getElementById('7').innerHTML = '';
+	document.getElementById('8').innerHTML = '';
+	document.getElementById('9').innerHTML = '';
 }
 
 function turnSequence() {
@@ -67,3 +89,23 @@ function turnSequence() {
 function writeMessage(message) {
 	//update the gameboard message with the message passed to it
 }
+
+// if (
+// 	squareChoice[0].textContent === 'X' &&
+// 	squareChoice[1].textContent === 'X' &&
+// 	squareChoice[2].textContent === 'X'
+// ) {
+// 	alert('Player 1 Wins!');
+// } else if (
+// 	squareChoice[3].textContent === 'X' &&
+// 	squareChoice[4].textContent === 'X' &&
+// 	squareChoice[5].textContent === 'X'
+// ) {
+// 	alert('Player 1 Wins!');
+// } else if (
+// 	squareChoice[6].textContent === 'X' &&
+// 	squareChoice[7].textContent === 'X' &&
+// 	squareChoice[8].textContent === 'X'
+// ) {
+// 	alert('Player 1 Wins!');
+// }
